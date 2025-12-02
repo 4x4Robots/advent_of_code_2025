456381b3ea518e60ef54a78ff839b5f7f1269307
Moritz <119802108+4x4Robots@users.noreply.github.com>
2025-12-02 22:01:33 +0100

    day 1 - part two counting drive-by zeros

--- src/day_1.py
+++ src/day_1.py
@@ -44,38 +44,6 @@ def _(input):
     return
 
 
-@app.cell
-def _(step):
-    def rotate_dial(start_position: int, lines: list[str], num_positions: int = 100) -> int:
-        """Rotate the position by the steps at the given index left or right. Return the number of zeros."""
-        position = start_position
-        num_zeros = 0
-    
-        for line in lines:
-            if line[0] == "L":
-                direction = -1
-            elif line[0] == "R":
-                direction = +1
-            else:
-                raise ValueError(f"Don't recognize step: {step}")
-            step = int(line[1:]) * direction
-    
-            position = (position + step) % num_positions
-            print(f"- The dial is rotated {line} to point at {position}.")
-            if position == 0:
-                num_zeros += 1
-
-        return num_zeros
-    return (rotate_dial,)
-
-
-@app.cell
-def _(input, rotate_dial):
-    result = rotate_dial(50, input, num_positions = 100)
-    result
-    return
-
-
 @app.function
 def rotate_dial_recursive(start_position: int, steps: list[str], index: int, num_zeros: int = 0, num_positions: int = 100) -> (int, int):
     """Rotate the position by the steps at the given index left or right. Return the new position."""
@@ -104,6 +72,87 @@ def rotate_dial_recursive(start_position: int, steps: list[str], index: int, num
 def _(input):
     end_position, num_zeros = rotate_dial_recursive(start_position=50, steps=input, index=0, num_positions=100)
     f"{end_position = }, {num_zeros = }"
+    return
+
+
+@app.cell
+def _(step):
+    def rotate_dial_part_one(start_position: int, lines: list[str], num_positions: int = 100) -> int:
+        """Rotate the position by the steps at the given index left or right. Return the number of zeros."""
+        position = start_position
+        num_zeros = 0
+    
+        for line in lines:
+            if line[0] == "L":
+                direction = -1
+            elif line[0] == "R":
+                direction = +1
+            else:
+                raise ValueError(f"Don't recognize step: {step}")
+            step = int(line[1:]) * direction
+    
+            position = (position + step) % num_positions
+            print(f"- The dial is rotated {line} to point at {position}.")
+            if position == 0:
+                num_zeros += 1
+
+        return num_zeros
+    return (rotate_dial_part_one,)
+
+
+@app.cell
+def _(input, rotate_dial_part_one):
+    result_one = rotate_dial_part_one(50, input, num_positions = 100)
+    result_one
+    return
+
+
+@app.cell
+def _(step):
+    def rotate_dial_part_two(start_position: int, lines: list[str], num_positions: int = 100) -> int:
+        """Rotate the position by the steps at the given index left or right. Return the number of zeros."""
+        position = start_position
+        num_zeros = 0
+        print(f"- The dial starts by pointing at {start_position}.")
+    
+        for line in lines:
+            if line[0] == "L":
+                direction = -1
+            elif line[0] == "R":
+                direction = +1
+            else:
+                raise ValueError(f"Don't recognize step: {step}")
+            step = int(line[1:]) * direction
+
+            # How many times was 0 passed?
+            zeros_passed = 0
+            if position + step > num_positions:
+                zeros_passed = (position + step) // num_positions
+                if position == 0:
+                    zeros_passed -= 1
+            if position + step < 0:
+                zeros_passed = -(position + step - num_positions)  // num_positions
+                if position == 0:
+                    zeros_passed -= 1
+            num_zeros += zeros_passed
+
+            # rotate to new position
+            position = (position + step) % num_positions
+
+            # don't count stopped at zero when overrotating (automatically included)
+            if position == 0: #and zeros_passed == 0:
+                num_zeros += 1
+        
+            print(f"- The dial is rotated {line} to point at {position}{'.' if zeros_passed == 0 else f'; during this rotation, it points at 0 {zeros_passed} times.'} {zeros_passed = } {num_zeros = }")
+
+        return num_zeros
+    return (rotate_dial_part_two,)
+
+
+@app.cell
+def _(input, rotate_dial_part_two):
+    result_part_two = rotate_dial_part_two(50, input, 100)
+    result_part_two
     return
 
 

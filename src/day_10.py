36a47972a41ba81f80825d848324d49b29d5b23b
Moritz <119802108+4x4Robots@users.noreply.github.com>
2025-12-22 15:21:02 +0100

    day 10 - read input

---
+++ src/day_10.py
@@ -0,0 +1,58 @@
+import marimo
+
+__generated_with = "0.18.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import copy
+    import math
+    import numpy as np
+    return (mo,)
+
+
+@app.cell
+def _():
+    PATH_FILE_EXAMPLE = "input/input_day_10_example.txt"
+    PATH_FILE_COMPLETE = "input/input_day_10_complete.txt"
+    return PATH_FILE_COMPLETE, PATH_FILE_EXAMPLE
+
+
+@app.cell
+def _(PATH_FILE_COMPLETE, PATH_FILE_EXAMPLE, ui_switch_complete_file):
+    path_file = PATH_FILE_COMPLETE if ui_switch_complete_file.value else PATH_FILE_EXAMPLE
+    path_file
+    return (path_file,)
+
+
+@app.cell
+def _(ui_switch_part_two):
+    is_part_two = ui_switch_part_two.value
+    return
+
+
+@app.cell
+def _(path_file):
+    def read_input(path_file):
+        with open(path_file, "r") as f:
+            input = f.readlines()
+            input = [line.strip() for line in input if line.strip() != ""]
+        return input
+
+    input = read_input(path_file)
+    input[:25]
+    return
+
+
+@app.cell
+def _(mo):
+    ui_switch_complete_file = mo.ui.switch(label="Use complete file", value=True)
+    ui_switch_part_two = mo.ui.switch(label="Part 2", value=True)
+    mo.hstack([ui_switch_complete_file, ui_switch_part_two])
+    return ui_switch_complete_file, ui_switch_part_two
+
+
+if __name__ == "__main__":
+    app.run()
